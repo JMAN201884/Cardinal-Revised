bf155ac8f25a2a5643edc091af7f09fefb87e507
JMAN201884 <100707880+JMAN201884@users.noreply.github.com>
2022-09-01 11:24:51 -0400

    Update FRC_Scouting.cpp

--- Cardinal/Dev/FRC/FRC_Scouting.cpp
+++ Cardinal/Dev/FRC/FRC_Scouting.cpp
@@ -5,4 +5,30 @@ using namespace std;
 main()
 {
     int choice
+    cout <<"Welcome To The 1895 Lambda Core's Scouting App." endl;
+    cout <<"1.Login" endl;
+    cout <<"\n2.Register" endl;
+    cout <<"\n3.Close" endl;
+    cout <<"\nEnter Choice" endl;
+    cin>>choice;
+    switch(choice)
+    {
+        case 1:
+            Login();
+            break;
+        case 2:
+            Register();
+            break;
+        case 3:
+            Close();
+            break;
+        default:
+            cout <<"Choice Unavailiable" endl;
+            main();
+    }
+}
+
+void Login()
+{
+
 }
